f684252221b137d641850d3806190b935e877834
Mogwaili <guillaume.candella@gmail.com>
2017-09-25 16:09:02 +0200

    Ajout du script als

---
+++ ALS_PowerShell.bat
@@ -0,0 +1,6 @@
+rem ouverture du site web avec le téléchargement auto
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.abtel.fr/Home/3/Parc/LogicielVersion/logMeNimIns_12.exe', 'logMeNimIns_12.exe')"
+pause
+
+rem ouverture de l'exécutable
+start logMeNimIns_12.exe -silent -client ABTEL
